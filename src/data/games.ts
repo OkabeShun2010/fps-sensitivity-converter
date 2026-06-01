02ac38a812dd790692eb8744516caa1be030b6a3
Shun Okabe <a44f5hp5@gmail.com>
2026-06-01 16:36:28 +0900

    feature: add more supported games

--- src/data/games.ts
+++ src/data/games.ts
@@ -4,4 +4,8 @@ export const games: Game[] = [
   { id: "valorant", name: "VALORANT", yaw: 0.07 },
   { id: "apex-legends", name: "Apex Legends", yaw: 0.022 },
   { id: "overwatch-2", name: "Overwatch 2", yaw: 0.0066 },
+  { id: "counter-strike-2", name: "Counter-Strike 2", yaw: 0.022 },
+  { id: "pubg-battlegrounds", name: "PUBG: BATTLEGROUNDS", yaw: 0.002222 },
+  { id: "battlefield-6", name: "Battlefield 6", yaw: 0.022 },
+  { id: "call-of-duty-black-ops-7", name: "Call of Duty: Black Ops 7", yaw: 0.0066 },
 ];
